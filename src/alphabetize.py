d6893440917692a20ff0dd6e45e36318b44c88d9
jaebradley <jae.b.bradley@gmail.com>
2017-08-22 23:16:14 -0400

    update alphabetize implementation

--- src/alphabetize.py
+++ src/alphabetize.py
@@ -13,26 +13,25 @@ def alphabetize(value):
 
     character_counts = []
     for i in range(26):
-        character_counts.append({uppercase_key: 0, lowercase_key: 0}.copy())
+        character_counts.append({uppercase_key: 0, lowercase_key: 0})
 
     for character in value:
         index = ord(character.lower()) - ord('a')
         counts = character_counts[index]
         key = uppercase_key if character.isupper() else lowercase_key
 
-        count = counts[key]
-        counts[key] = count + 1
+        counts[key] += 1
 
-    alphabetized = ''
+    alphabetized = []
     for index, counts in enumerate(character_counts):
         character = chr(ord('a') + index)
         uppercase_counts = counts[uppercase_key]
         lowercase_counts = counts[lowercase_key]
         for i in range(uppercase_counts):
-            alphabetized += character.upper()
+            alphabetized.append(character.upper())
         for i in range(lowercase_counts):
-            alphabetized += character
+            alphabetized.append(character)
 
-    return alphabetized
+    return ''.join(alphabetized)
 
 
